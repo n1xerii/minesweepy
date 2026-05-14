e2825de520b09bebc418ddceeb52f014d1b810ee
LNixerii <119566245+n1xerii@users.noreply.github.com>
2026-05-14 17:35:19 +0300

    Update Cell.cs

--- Cell.cs
+++ Cell.cs
@@ -4,8 +4,12 @@ namespace minesweepy;
 
 public class Cell
 {
-    private int id;
-    private Vector2 position;
-    private bool revealed;
-    private bool isBomb;
+    public int id { get; set; }
+    public int x { get; set; }
+    public int y { get; set; }
+    
+    public bool revealed{ get; set; }
+    public bool isBomb{ get; set; }
+    public int neighbors { get; set; }
+    public bool isFlagged{ get; set; }
 }
