0167b49700ec76d067b36b34237285f3db772a49
nixerii <119566245+n1xerii@users.noreply.github.com>
2026-05-15 00:22:31 +0300

    Grid, rows and columns

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -5,9 +5,50 @@ namespace minesweepy;
 public partial class MainWindow : Window
 {
     //private Cell[,] cells;
+    private int rows = 10;
+    private int cols = 10;
+    
     
     public MainWindow()
     {
         InitializeComponent();
+        SetBoard(rows, cols);
     }
+
+    public void SetBoard(int givenRows, int givenCols)
+    {
+        BoardGrid.RowDefinitions.Clear();
+        BoardGrid.ColumnDefinitions.Clear();
+        BoardGrid.Children.Clear();
+
+        for (int r = 0; r < rows; r++)
+        {
+            BoardGrid.RowDefinitions.Add(new RowDefinition(GridLength.Star));
+        }
+        for (int c = 0; c < cols; c++)
+        {
+            BoardGrid.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Star));
+        }
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                var button = new Button();
+                
+                Grid.SetRow(button, r);
+                Grid.SetColumn(button, c);
+                button.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Stretch;
+                button.VerticalAlignment = Avalonia.Layout.VerticalAlignment.Stretch;
+
+                BoardGrid.Children.Add(button);
+            }
+        }
+    }
+    
+    //private void CellClicked(object? sender, RoutedEventArgs e)
+    //{
+    //    if (sender is Button btn)
+    //        btn.Content = "•";
+    //}
 }
