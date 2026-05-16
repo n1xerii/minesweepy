72c72576a6eb1e34e461fc328188211f1acd3b03
LNixerii <119566245+n1xerii@users.noreply.github.com>
2026-05-16 16:51:13 +0300

    Simplified cell class

--- Cell.cs
+++ Cell.cs
@@ -4,12 +4,8 @@ namespace minesweepy;
 
 public class Cell
 {
-    public int id { get; set; }
-    public int x { get; set; }
-    public int y { get; set; }
-    
     public bool revealed{ get; set; }
     public bool isBomb{ get; set; }
     public int neighbors { get; set; }
-    public bool isFlagged { get; set; }
+    public bool flagged { get; set; }
 }
