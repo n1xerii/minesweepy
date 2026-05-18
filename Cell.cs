0c7dd44f8e12118524395b1690d07803b692a6f2
nixerii <119566245+n1xerii@users.noreply.github.com>
2026-05-18 12:10:44 +0300

    Remove neighbors list in preparation

--- Cell.cs
+++ Cell.cs
@@ -9,6 +9,5 @@ public class Cell
     public int myCol { get; set; }
     public bool revealed{ get; set; }
     public bool isBomb{ get; set; }
-    public List<Cell> neighbors = new List<Cell>();
     public bool flagged { get; set; }
 }
